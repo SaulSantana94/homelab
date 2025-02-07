208a9b3199e8676a4bed81364475902181a422d8
Daniel Boluda <72820621+Daniel-Boluda@users.noreply.github.com>
2025-02-07 14:12:21 +0100

    update nvidia-watcher

--- system/nvidia-watcher/src/dockerbuild.sh
+++ system/nvidia-watcher/src/dockerbuild.sh
@@ -1,3 +1,3 @@
 docker build -t nvidia-watcher -f Dockerfile .
-docker tag nvidia-watcher bolferdocker/nvidia-watcher:0.0.2
-docker push bolferdocker/nvidia-watcher:0.0.2
+docker tag nvidia-watcher bolferdocker/nvidia-watcher:0.0.3
+docker push bolferdocker/nvidia-watcher:0.0.3
